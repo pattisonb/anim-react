e5662490cfc17de1724392959a104a402d5c78b8
Manov Jain <47869311+ManovJain@users.noreply.github.com>
2023-06-07 21:15:02 -0700

    initaly framework for UI, homepage, navbar, and privacy policy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from './Pages/HomePage';
+// import { HomePage } from './Pages/HomePage';
 import PrivacyPolicy from "./Pages/PrivacyPolicy";
+import Navbar from "./Components/Navbar";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="" element={<HomePage />}></Route>
         <Route path="/privacyPolicy" element={<PrivacyPolicy />}></Route>
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,15 +1,42 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import logo from '../logo.svg';
+// import '../App.css';
+
+// export class HomePage extends Component { 
+//     render()  {
+//     return (
+            // <div className="App">
+            // <header className="App-header">
+            //     <img src={logo} className="App-logo" alt="logo" />
+            // </header>
+            // </div>
+//         );
+//     }
+// }
+
+import React from 'react';
 import logo from '../logo.svg';
+import './HomePage.css';
 import '../App.css';
 
-export class HomePage extends Component { 
-    render()  {
-    return (
-            <div className="App">
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <div className="left-div">
+        <div>
+            <h1>ANIM</h1>
+            <h2>SUSTAINABILITY MADE SIMPLE</h2>
+        </div>
+      </div>
+      <div className="right-div">
+      <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
             </header>
             </div>
-        );
-    }
-}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
