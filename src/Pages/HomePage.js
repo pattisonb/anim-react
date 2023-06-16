a7cf6baa4b4b85826314ad9d97a42d805f1120fa
Manov Jain <47869311+ManovJain@users.noreply.github.com>
2023-06-16 09:07:00 -0700

    phone image

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from '../logo.svg';
 import { Image } from 'react-bootstrap';
-import appStore from "../Assets/appStoreDownload.png"
+import appStore from "../Assets/appStoreDownload.png";
+import animDisplay from "../Assets/animPhone.png"
+
 import './HomePage.css';
 import '../App.css';
 
@@ -19,7 +21,12 @@ const HomePage = () => {
       </div>
       <div className="right-div">
       <div className="App">
-                <img src={logo} className="App-logo" alt="logo" />
+              <img
+            src={animDisplay}
+            className="App-logo"
+            alt="logo"
+            style={{ height: '800px' }} // Adjust the height value as per your requirement
+          />
             </div>
       </div>
     </div>
